efa104ae3e7938021c4ba6f9de5bb6d5a49959e0
League Student <league@iMac.attlocal.net>
2019-11-03 11:28:40 -0800

    test

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
 
 public class VoteProcessor {
-
+ public String calculateElectionWinner(ArrayList <String> votes) {
+	for(int i=0; i<votes.size(); i++) {
+		if(votes.get(i).equalsIgnoreCase("pope francis")) {
+		votes{i}=votes.get(i).ign
+	}
+}
 }
